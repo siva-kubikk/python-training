5f110437447b075779b8cdace74f0912ec5ceaee
siva-kubikk <siva@kubikk.io>
2023-11-25 15:16:37 -0800

    13

---
+++ 13-passwd-gen.py
@@ -0,0 +1,17 @@
+import random, string
+
+# a = int(input("Enter password length: "))
+
+a = random.randint(10, 30)
+p = []
+
+
+for i in range(0, a):
+    special_char = random.choice(string.punctuation)
+    number = str(random.randint(0, 9))
+    upper_case = random.choice(string.ascii_uppercase)
+    lower_case = random.choice(string.ascii_lowercase)
+    # print(i)
+    p.append(random.choice((special_char, number, upper_case, lower_case)))
+
+print("".join(random.sample(p, a)))
