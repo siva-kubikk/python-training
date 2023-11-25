36eb69b098aba1ede6b4c69a011f54a2e6020f6e
siva-kubikk <siva@kubikk.io>
2023-11-25 15:17:32 -0800

    13

--- 13-passwd-gen.py
+++ 13-passwd-gen.py
@@ -11,7 +11,6 @@ for i in range(0, a):
     number = str(random.randint(0, 9))
     upper_case = random.choice(string.ascii_uppercase)
     lower_case = random.choice(string.ascii_lowercase)
-    # print(i)
     p.append(random.choice((special_char, number, upper_case, lower_case)))
 
 print("".join(random.sample(p, a)))
