d6007381bbbdf93261919f5e473f043e3a364665
siva-kubikk <siva@kubikk.io>
2023-11-24 18:22:32 -0800

    bank

--- 3-banking.py
+++ 3-banking.py
@@ -4,7 +4,7 @@ bank = {
 }
 
 def bank_deposit(account, amount):
-    if amount > 0:
+    if amount > 0: # Checking if deposit is greater than zero
         account['balance'] += amount
         account['transactions'].append(f"Deposit: ${amount}")
         print(f"Depositing ${amount}.\nNew balance is ${account['balance']}.\n")
@@ -12,16 +12,20 @@ def bank_deposit(account, amount):
         print("Invalid deposit amount.")
 
 def bank_withdraw(account, amount):
-    if 0 < amount <= account['balance']:
+    # if 0 < amount <= account['balance']:
+    if amount > 0:
+      if amount <= account['balance']:
         account['balance'] -= amount
         account['transactions'].append(f"Withdrawal: ${amount}")
         print(f"Withdrawing ${amount}.\nNew balance is ${account['balance']}.\n")
+      else:
+         print(f"Excess withdrawal amount ${amount}. Balance is only ${account['balance']}")
     else:
-        print("Invalid or excessive withdrawal amount.")
+        print(f"Withdrawal amount ${amount} cannot be less than $0.")
 
 
 bank_deposit(bank, 1000)
-bank_withdraw(bank, 5000)
+bank_withdraw(bank, -10)
 
 # Print transaction history
 print("Transaction History:", bank['transactions'])
